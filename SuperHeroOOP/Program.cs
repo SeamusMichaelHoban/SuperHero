afb79cb22106a82114bda26aa07b6de228c0d844
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-05-30 12:11:24 -0400

    added some fields and constructors

---
+++ SuperHeroOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Superhero batman = new Superhero("Batman" , true);
+            Console.WriteLine(batman.Name);
+            Console.WriteLine("Is your superhero a superhero??  {0}", batman.IsSuperhero);
+
+            string[] supermanAbilities = new string[3]; 
+
+            Superhero superman = new Superhero("Superman", "Lex Luthor", "Clark Kent", supermanAbilities);
+            Console.WriteLine(superman.Nemesis);
+            Console.WriteLine(superman.SecretIdentity);
+        }
+
+    }
+}
---
+++ SuperHeroOOP/Superhero.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroOOP
+{
+    class Superhero
+    {// declaring fields
+        private string name;
+        private string nemesis;
+        private string secretIdentity;
+        private string[] specialAbilities;
+        private bool isSuperhero;
+
+        //declaring properties
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Nemesis
+        {
+            get { return this.nemesis; }
+            set { this.nemesis = value; }
+        }
+
+        public string SecretIdentity
+        {
+            get { return this.secretIdentity; }
+            set { this.secretIdentity = value; }
+        }
+
+        public bool IsSuperhero
+        {
+            get { return this.isSuperhero; }
+            set { this.isSuperhero = value; }
+        }
+        //public int Weight
+        //{
+        //    get { return this.weight; }
+        //    set { this.weight = value; }
+        //}
+
+        //creating 3 constructor methods
+        public Superhero()
+        {
+
+        }
+        
+        public Superhero(string name, bool isSuperhero)
+        {
+            this.isSuperhero = isSuperhero;
+            this.name = name;
+        }
+
+        public Superhero( string name, string nemesis, string secretIdentity, string[] specialAbilities)
+        {
+            isSuperhero = true;
+            this.name = name;
+            this.nemesis = nemesis;
+            this.secretIdentity = secretIdentity;
+            this.specialAbilities = specialAbilities;
+        }
+
+        
+
+
+
+
+    }
+}
